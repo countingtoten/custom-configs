06db30ecc56c0582dc3e4a389f7111aed27665cd
Jim Weinert <countingtoten@gmail.com>
2018-08-19 21:25:46 -0500

    Use the latest ruby by default

--- ruby.sh
+++ ruby.sh
@@ -4,4 +4,8 @@ export GEM_PATH="$GEM_HOME:/usr/lib/ruby/gems/2.3.0/"
 export PATH="$PATH:$GEM_HOME/bin"
 
 source "$(brew --prefix)/share/chruby/chruby.sh"
+
+# Set the ruby version to the latest installed
+chruby ruby
+
 source "$(brew --prefix)/share/chruby/auto.sh"
