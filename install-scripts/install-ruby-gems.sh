196c544575a1db4e46fe75cffe55cb7007b4d07e
Jim Weinert <countingtoten@gmail.com>
2018-08-19 20:02:40 -0500

    Add install script for common ruby gems

---
+++ install-scripts/install-ruby-gems.sh
@@ -0,0 +1,8 @@
+# After installing the latest ruby run this script to install the most commonly
+# used gems.
+#
+# sudo will install ruby to /opt/rubies/
+# sudo ruby-install ruby
+# bundler config vendor/
+
+gem install rails bundler octopress scss_lint reek rogue rubocop
